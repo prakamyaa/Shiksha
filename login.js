d937ae94a0e0841a92984481c2f49ed486af152f
Utsav Pandey <126980036+UtsavMNNIT@users.noreply.github.com>
2023-03-19 13:42:57 +0530

    login.js
    Javascript file to create login page.

---
+++ login.js
@@ -0,0 +1,9 @@
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+ 
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
